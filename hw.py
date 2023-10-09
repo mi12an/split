a2f196b89ec5c540330e5336415aabf842a833d8
mi12an <67796062+mi12an@users.noreply.github.com>
2023-10-09 10:23:51 +0200

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+def print_hello_world_art():
+    art = '''
+      H   H  EEEEE  L          L           OOO       W   W  OOO  RRRR  L      DDDD  !!
+      H   H  E      L          L         O     O      W W W O   O R   R L      D   D !! 
+      HHHHH  EEEE   L          L         O     O      W W W O   O RRRR  L      D   D !! 
+      H   H  E      L          L         O     O  ,,   W W  O   O R  R  L      D   D    
+      H   H  EEEEE  LLLLL      LLLLL      OOO  ,,     W W   OOO  R   R LLLLL  DDDD  !!
+    '''
+    print(art)
+
+print_hello_world_art()
